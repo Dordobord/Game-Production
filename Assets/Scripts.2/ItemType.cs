e0122e4ee84e4cda1559fcb0cdbf1aafdbf0e069
Dordobord <angelo.dorado@ciit.edu.ph>
2026-02-19 16:35:01 +0800

    Merged 2 builds
    -Added cooking mechanics
    - Added playerinventory
    - Was dishes mechanics
    - AssemblyStation
    - Coffee mechanics

---
+++ Assets/Scripts.2/ItemType.cs
@@ -0,0 +1,17 @@
+public enum ItemType
+{
+    RawBeef,
+    CookedBeef,
+    Bun,
+    Plate,
+    DirtyPlate,
+    RawChicken,
+    CookedChicken,
+    RawFries,
+    CookedFries,
+    PlatedBurger,
+    PlatedChicken,
+    PlatedFries,
+    CoffeeCup
+
+}
